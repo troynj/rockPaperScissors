383b7694c9e8d33a47be342875bab49e68da41d3
Fayven <fayven.amelga@gmail.com>
2022-12-21 15:39:00 -0500

    Add comments

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
+// Retrieve user from the DOM
 var generateMove = document.querySelector(".generate");
 
 generateMove.addEventListener("click", playRound);
+// Start game
+function startGame() {
+    
+}
 
 function playRound() {
+    // Start game
     //getMyMove 
-    getAIMove()
+    getAIMove() //getAIMove() 
     //compareMoves
     //display winner
     // keeping score
+    // Replay or quit
 
 }
-
+//getMyMove
 function getMyMove() {
     //depending on eventlistener, produce number
     //return number
 }
-
+//getAIMove
 function getAIMove() {
     var aiChoice = Math.floor(Math.random() * 3);
 }
-
+//compareMoves
 function compareMoves () {
     //rock = 1
     //paper = 2
@@ -68,9 +75,11 @@ function compareMoves () {
 
     //return who won
 }
-
+//get winner
 function getWinner () {
 
     // ai or i won
     //rps beats rps
 }
+    // keeping score
+    // Replay or quit
