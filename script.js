9c5cf6d79541e46305c38701cfdf0d56be09b6a9
Fayven <fayven.amelga@gmail.com>
2022-12-21 15:08:48 -0500

    Add eventlistener

---
+++ script.js
@@ -0,0 +1,7 @@
+var generateMove = document.querySelector(".generate");
+
+generateMove.addEventListener("click", playRound);
+
+function getAImove() = {
+    var aiChoice = Math.floor(Math.random() * 3);
+}
